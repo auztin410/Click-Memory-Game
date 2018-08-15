ef26d2a8df946a1f9ccefc1ca6ad39a6d68a703b
auztin410 <auztin410@gmail.com>
2018-08-15 08:52:19 -0400

    Counter for score is working.

--- click-memory-game/src/components/FriendCard/Counter.js
+++ click-memory-game/src/components/FriendCard/Counter.js
@@ -4,13 +4,36 @@ import Characters from "../../friends.json";
 
 console.log(Characters);
 
+
 class Counter extends React.Component {
     state = {
         count: 0
     };
 
+    // Shuffle = (o) => {
+    //     for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    //     return o;
+    // };
+    
+    
+
     handleClick = () => {
-        this.setState({ count: this.state.count + 1 });
+        this.setState({count: this.state.count + 1})
+        console.log(this.state.count);
+        console.log(this);
+        // if(this.character.clicked === 1){
+        //     alert("You clicked the same one twice. Try again!");
+        // }
+        // else if(this.character.click < 1){
+        //     this.character.clicked = 1
+        //     this.setState({ count: this.state.count + 1 });
+        //     if(this.state.count = 8){
+        //         alert("You won!");
+        //     }
+        //     else{Shuffle(friends);}
+        // }
+        
+        
     }
 
     
@@ -18,6 +41,9 @@ class Counter extends React.Component {
     render() {
         return (            
                 <div className="img-container">
+                <h1>
+                    Score: {this.state.count}
+                </h1>
                 {Characters.map((character) => {
                     return(
                         <img
@@ -26,10 +52,12 @@ class Counter extends React.Component {
                         key={character.name}
                         height="200px"
                         width="200px"
+                        onClick = {this.handleClick}
+                        clicked = "0"
                     />
                     )
                     
-                    // {console.log(character.image)}
+                    
                 })}
                 </div>              
             
